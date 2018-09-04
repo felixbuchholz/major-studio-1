8d25c857f1d0aa9de1fe449347ec422a6732bfb2
felixbuchholz <felixbuchholz@gmx.net>
2018-09-04 14:30:56 -0400

    Balls are now in an array, can be added with the arrow up key and they speed up on contact

--- inClassProblemSolving/02-soloPong/sketch.js
+++ inClassProblemSolving/02-soloPong/sketch.js
@@ -2,85 +2,107 @@ var w = 400;
 var h = 600;
 var fr = 100;
 
-var r = 0;
+class Ball {
+  constructor() {
+    this.r = Math.floor(Math.random() * 5) + 10;
+    this.xPos = Math.floor(Math.random() * w);
+    this.yPos = Math.floor(Math.random() * (h - marginBottom*3 - rectHeight));
+    this.xSpeed = Math.floor(Math.random() * 10) - 9;
+    this.ySpeed = Math.floor(Math.random() * 10) - 9;
+    while (this.ySpeed == 0) {
+      this.ySpeed = Math.floor(Math.random() * 10) - 9;
+    };
+    this.display = function() {
+      ellipse(this.xPos, this.yPos, this.r, this.r);
+    };
+    this.move = function() {
+      this.leftWallContact = this.xPos <= 0 + Math.floor(this.r/2);
+      this.rightWallContact = this.xPos >= w - Math.floor(this.r/2)
+      this.oppositeWallContact = this.yPos <= 0 + Math.floor(this.r/2);
+      this.rectContact = (this.xPos <= mouseX + rectWidth/2) && (this.xPos >= mouseX - rectWidth/2) && (this.yPos >= (h-(marginBottom+rectHeight))) && (this.yPos <= (h-(marginBottom+rectHeight-(this.ySpeed+1))));
+      if (this.leftWallContact || this.rightWallContact) {
+        this.xSpeed *= -1;
+      };
+      if (this.oppositeWallContact || this.rectContact) {
+        this.ySpeed *= -1;
+        if (this.rectContact) {
+          contactCounter++;
+          moveTester = 0;
+          rotateTest *= -1;
+          this.ySpeed *= 1.1;
+          //TODO Spin here
+          this.spin = map(xSpeed, -1.1, 1.1, rectLeft, rectRight);
+          this.xSpeed *= 1.1;
+        };
+      };
+      this.xPos += this.xSpeed;
+      this.yPos += this.ySpeed;
+    }
+  }
+}
+
+var balls = [];
+
+var rectHeight = 15;
 var marginBottom = 0;
 var rectWidth = 0;
+var rectLeft, rectRight;
 var contactCounter = 0;
-var xPos = 0;
-var yPos = 0;
-var xSpeed = 0;
-var ySpeed = 0;
 var heightGainOnContact = 0;
 var moveTester = 0;
 var rotateTest = -1;
-console.log(r, marginBottom, xPos, yPos, xSpeed, ySpeed, rectWidth);
 
 function setVariables() {
-  r = Math.floor(Math.random() * 5) + 10;
   contactCounter = 0;
   marginBottom = contactCounter * heightGainOnContact + 20;
-  xPos = Math.floor(Math.random() * w);
-  yPos = Math.floor(Math.random() * (h - marginBottom*3 - r));
-  xSpeed = Math.floor(Math.random() * 10) - 9;
-  ySpeed = Math.floor(Math.random() * 10) - 9;
-  while (ySpeed == 0) {
-    console.log('happened');
-    ySpeed = Math.floor(Math.random() * 10) - 9;
-  }
   rectWidth = Math.floor(Math.random() * 50) + 20;
   heightGainOnContact = Math.floor(Math.random() * 50) +10;
   moveTester = heightGainOnContact;
   rotateTest = -1;
-
-    console.log(r, marginBottom, xPos, yPos, xSpeed, ySpeed, rectWidth, heightGainOnContact);
 }
 
 setVariables();
 
 function mouseClicked(){
   setVariables();
+  for (var i = 0; i < 1; i++) {
+    balls[i] = new Ball();
+  }
 }
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    balls.push(new Ball())
+    console.log(balls.length);
+  }
+}
+
 
 function setup() {
   createCanvas(w, h);
   frameRate(fr);
+  balls[0] = new Ball();
 }
 
 function draw() {
   background(200);
-
-  xPos += xSpeed;
-  yPos += ySpeed;
 
   push();
   if (rotateTest > 0) {
     translate(width, height);
     rotate(PI);
   }
-
-  ellipse(xPos, yPos, r, r);
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].display();
+    balls[i].move();
+  }
   rectMode(CENTER);
-  rect(mouseX, h-marginBottom, rectWidth, r);
+  rect(mouseX, h-marginBottom, rectWidth, rectHeight);
+  rectLeft = mouseX - rectWidth/2;
+  rectRight = mouseX + rectWidth/2;
+  textAlign(CENTER);
+  text(contactCounter, mouseX, h-marginBottom+rectHeight/2-1)
   pop();
-
-  var leftWallContact = xPos <= 0 + Math.floor(r/2);
-  var rightWallContact = xPos >= w - Math.floor(r/2)
-
-  if (leftWallContact || rightWallContact) {
-    xSpeed *= -1;
-  }
-
-  var oppositeWallContact = yPos <= 0 + Math.floor(r/2);
-  var rectContact = (xPos <= mouseX + rectWidth/2) && ( xPos >= mouseX - rectWidth/2) && (yPos >= (h-(marginBottom+r))) && (yPos <= (h-(marginBottom+r-(ySpeed+1))));
-
-  if (oppositeWallContact || rectContact) {
-    ySpeed *= -1;
-    if (rectContact) {
-      contactCounter++;
-      moveTester = 0;
-      rotateTest *= -1;
-    }
-  }
 
   setTimeout(function() {
       if (moveTester < heightGainOnContact) {
@@ -90,9 +112,7 @@ function draw() {
         marginBottom = contactCounter * heightGainOnContact + moveTester;
       };
     },
-    (1000/fr*-1/ySpeed)+20/heightGainOnContact
+    // Previoulsy the value 5 in the next line was the ySpeed variable of the Ball, to make it dependent on the
+    (1000/fr*-1/5)+20/heightGainOnContact
   )
-
 }
-// Todo: implement a switch to flip the canvas
-// if rectContact: es startet ein Ding, checkt ob wir heightGainOnContact haben, und wenn nicht, dann ++
